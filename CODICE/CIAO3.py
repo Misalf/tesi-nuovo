c7e6f66b925140cf52c3f6af505714ce024814d2
deltapir <deltapirandello@live.com>
2017-05-13 18:06:41 +0200

    Un po' di reti neurali

--- CODICE/CIAO3.py
+++ CODICE/CIAO3.py
@@ -38,9 +38,12 @@ r=0
 for row in outputexcel:
     arrayoutput=[0 for x in range(r+1)]
     arrayoutput[r]=row[0].value             #AGGIUNGO I VALORI DA EXCEL NELLA MATRICE
+    r=r+1
+r=0
+for row in outputexcel:
+    arrayoutput[r]=row[0].value             #AGGIUNGO I VALORI DA EXCEL NELLA MATRICE
     print (arrayoutput[r] ,',', r)
     r=r+1
-
 #INSERISCE NELLA MATRICE I VALORI CODIFICATI
 print("INIZIO CODIFICA")
 r=0                        
@@ -65,11 +68,37 @@ for row in inputexcel:
             matriceproteinacodificata[7][r]=cell.value
         c=c+1
     r=r+1
-    if r==RigMAX:
-        break
+    #if r==RigMAX:
+      #  break
 
 print ("INIZIO MATRICE CODIFICATA")
 ri,ci=0,0
 for row in range(0,r-1):
     for cell in range(0,c-1):
         print (matriceproteinacodificata[cell][row])
+
+X=np.array(matriceproteinacodificata).T
+Y=np.array(arrayoutput)
+
+#SI INIZIA A COPIARE
+np.random.seed(1)
+
+#INIZIALIZZO I PESI CASUALMENTE CON MEDIA 0
+syn0 = 2*np.random.random((8,1)) - 1
+
+for iter in range(10000):
+    #forward propagation
+    p0=X
+    p1=nonlin(np.dot(p0,syn0))
+
+    #quanto è l'errore?
+    p1_errore=Y-p1
+
+    #moltiplica quanto abbiamo sbagliato per la derivata della sigmoide al valore in p1
+    p1_delta=p1_errore*nonlin(p1,True)
+
+    #aggiorna i pesi
+    syn0+=np.dot(p0.T,p1_delta)
+
+print ("Output after training:")
+print (p1)
