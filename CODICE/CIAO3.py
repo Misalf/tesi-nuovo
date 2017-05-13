113d59727f62f188183377d5bcae7967b8b32615
deltapir <deltapirandello@live.com>
2017-05-13 17:15:30 +0200

    Tolto le cose inutili

--- CODICE/CIAO3.py
+++ CODICE/CIAO3.py
@@ -7,17 +7,6 @@ def nonlin(x,deriv=False):
         return x*(1-x)
     return 1/(1+np.exp(-x))
 
-def CODIFICA(ARRAY,INP):
-    print (ARRAY.index(INP))
-    return ARRAY.index(INP)
-#    i=0
-#    for CONT in ARRAY:
-#        if INP==CONT:
-#            print ("ciao misa") 
-#            return i
-#        else:
-#            i=i+1
-
 AMMINOACIDI=['MET', 'LYS', 'PRO', 'THR', 'VAL', 'ALA', 'ASN', 'GLU', 'PRO', 'TYR','ARG']
 ALFABETO=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
@@ -25,15 +14,12 @@ ALFABETO=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
 #matrice input
 FirstInput = openpyxl.load_workbook('first.xlsx')
 sheet=FirstInput.active
-#sheet=FirstInput.get_sheet_names()
-#importo matrice input
 inputexcel = sheet['B2':'I30']
-#col_range=sheet['B:I']
-#print (inputexcel)
 
 
+#CREA LA MATRICE CON TUTTI
 r=0
-for row in inputexcel:          #CREA LA MATRICE CON TUTTI
+for row in inputexcel:          
     c=0
     RigMAX=r
     #print (RigMAX)
@@ -43,16 +29,15 @@ for row in inputexcel:          #CREA LA MATRICE CON TUTTI
         #print (c,',',r)
         c=c+1
     r=r+1
-print("INIZIO CODIFICA")
-r=0                        #INSERISCE NELLA MATRICE I VALORI CODIFICATI
 
-#print("Stampa:",matriceproteinacodificata[7][0])
+#INSERISCE NELLA MATRICE I VALORI CODIFICATI
+print("INIZIO CODIFICA")
+r=0                        
 for row in inputexcel:
     c=0
     for cell in row:
         if c==0:
             matriceproteinacodificata[0][r]=AMMINOACIDI.index(cell.value) #CODIFICA(AMMINOACIDI,cell.value)
-            #print (AMMINOACIDI.index(cell.value))
         elif c==1:
             matriceproteinacodificata[1][r]=ALFABETO.index(cell.value) #CODIFICA(ALFABETO,cell.value)
         elif c==2:
@@ -67,7 +52,6 @@ for row in inputexcel:
             matriceproteinacodificata[6][r]=ALFABETO.index(cell.value) #CODIFICA(ALFABETO,cell.value)
         elif c==7:
             matriceproteinacodificata[7][r]=cell.value
-        #print (matriceproteinacodificata[r][c]) #<— ricorda di toglierlo
         c=c+1
     r=r+1
     if r==RigMAX:
@@ -78,6 +62,3 @@ ri,ci=0,0
 for row in range(0,r-1):
     for cell in range(0,c-1):
         print (matriceproteinacodificata[cell][row])
-        #print (row ,",", cell)
-        #ci=ci+1
-    #ri=ri+1
